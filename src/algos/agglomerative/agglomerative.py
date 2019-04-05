0171afa39acda19480996b02941e36cef191624d
Madhav Nandipati <mnandipa@gmail.com>
2019-04-05 13:47:55 -0400

    created agglo code

--- src/algos/agglomerative/agglomerative.py
+++ src/algos/agglomerative/agglomerative.py
@@ -1,9 +1,21 @@
-from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
 import numpy as np
+import pandas as pd
+import scipy.cluster.hierarchy as shc
+import pdb
 
 data_full = np.genfromtxt('../../../data/data_v1/final/training_data_v1_final.csv', delimiter=',')
+
 # Remove the first two columns which include data
 training = data_full[:,2:]
-kmeans = KMeans(n_clusters=10).fit(training)
-print(kmeans.labels_)
-print(kmeans.inertia_)
+#pdb.set_trace()
+
+#Run clustering algorithm
+cluster = AgglomerativeClustering(n_clusters=2, affinity='euclidean', linkage='ward')  
+cluster.fit_predict(training) 
+print(cluster.labels_)
+
+#Plots for dendograms
+#plt.figure(figsize=(10, 7))  
+#plt.title("Customer Dendograms")  
+#dend = shc.dendrogram(shc.linkage(X, method='ward')) 
