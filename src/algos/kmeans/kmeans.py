c4849dc4dd7330e0a0bf643bd663109de1340afd
Robert Gifford <robertgif@gmail.com>
2019-04-05 11:47:52 -0400

    Fix directory structure

--- src/algos/kmeans/kmeans.py
+++ src/algos/kmeans/kmeans.py
@@ -1,7 +1,7 @@
 from sklearn.cluster import KMeans
 import numpy as np
 
-data_full = np.genfromtxt('../../data/data_v1/final/training_data_v1_final.csv', delimiter=',')
+data_full = np.genfromtxt('../../../data/data_v1/final/training_data_v1_final.csv', delimiter=',')
 # Remove the first two columns which include data
 training = data_full[:,2:]
 kmeans = KMeans(n_clusters=10).fit(training)
