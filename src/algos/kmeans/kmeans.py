8b9ae3a16e2579faed73686d8d89d99ed685b47e
Robert Gifford <robertgif@gmail.com>
2019-04-17 16:20:09 -0400

    Add visualization code
    * Demonstartion for using it in kmeans.py
    * TODO: remove calling code from kmeans.py, turn kmeans into a function,
    add a new higherlevel file to run both kemans and the visualizer so we
    can swap in new algorithms other than kmeans
    * Update .gitignore to include generated png files

--- src/algos/kmeans/kmeans.py
+++ src/algos/kmeans/kmeans.py
@@ -1,12 +1,27 @@
 from sklearn.cluster import KMeans
 import numpy as np
+import sys
+
+sys.path.append('../../util/')
+
+import visualizer as viz
+import pca_util
 
 data_full = np.genfromtxt('../../../data/data_v1/final/training_data_v1_final.csv', delimiter=',')
 # Remove the first two columns which include data
 training = data_full[:,2:]
-kmeans = KMeans(n_clusters=10).fit(training)
+
+# TODO:
+#   Turn this into a function, num_components and k should be arguments
+num_components = 5;
+k = 10;
+pca, trans_data = pca_util.get_pca(num_components, training)
+kmeans = KMeans(n_clusters=k).fit(trans_data)
 print(kmeans.labels_)
 print(kmeans.inertia_)
+
+viz.visualize(trans_data, kmeans.labels_, two_d = True, three_d = False)
+viz.visualize(trans_data, kmeans.labels_, two_d = False, three_d = True)
 
 # For debugging:
 #import pdb; pdb.set_trace()
