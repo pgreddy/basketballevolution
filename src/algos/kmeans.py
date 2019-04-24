e03c7eea6b6c095081db2f3b686405b7a2de4268
Pranav Reddy <preddy@sas.upenn.edu>
2019-04-24 18:54:56 -0400

    Added complex feature generation and configurations

--- src/algos/kmeans.py
+++
@@ -1,9 +0,0 @@
-from sklearn.cluster import KMeans
-import numpy as np
-
-data_full = np.genfromtxt('../../data/data_v1/final/training_data_v1_final.csv', delimiter=',')
-# Remove the first two columns which include data
-training = data_full[:,2:]
-kmeans = KMeans(n_clusters=10).fit(training)
-print(kmeans.labels_)
-print(kmeans.inertia_)
