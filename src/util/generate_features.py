c8e647821a5000da128a87c74f5e599109aad66d
Pranav Reddy <preddy@sas.upenn.edu>
2019-04-26 10:54:42 -0400

    data set v2 generated

--- src/util/generate_features.py
+++ src/util/generate_features.py
@@ -25,12 +25,18 @@ with open(sys.argv[4] + 'training_data_' + sys.argv[5] + '.csv', 'w') as trainin
         data_suffix = '.' + data_file_path[1]
 
         # READ AND PARSE PLAYER ATTRIBUTES
-        players = {}
-        conditionalplayers = [dict() for x in range(len(conditionals))]
+        primary_players = {}
+        secondary_players = {}
+        conditional_primary_players = [dict() for x in range(len(conditionals))]
+        conditional_secondary_players = [dict() for x in range(len(conditionals))]
         with open(sys.argv[3] + data_prefix + data_suffix) as eventfile:
             for line in eventfile:
                 els = line.split(",")
                 p1id = int(els[0])
+                try:
+                    p2id = int(els[1])
+                except:
+                    p2id = -1
                 event = int(els[2])
                 attr = []
                 for x in els[0:]:
@@ -39,32 +45,46 @@ with open(sys.argv[4] + 'training_data_' + sys.argv[5] + '.csv', 'w') as trainin
                     except:
                         attr.append(-1)
 
-                # PARSE FOR GENERAL FILE
-                if p1id in players.keys():
-                    players[p1id][event] = players[p1id][event] + 1
+                # PARSE FOR GENERAL FILE FOR PLAYER 1
+                if p1id in primary_players.keys():
+                    primary_players[p1id][event] = primary_players[p1id][event] + 1
                 else:
-                    players[p1id] = [0] * (6)
-                    players[p1id][event] = players[p1id][event] + 1
-                
+                    primary_players[p1id] = [0] * (13)
+                    primary_players[p1id][event] = primary_players[p1id][event] + 1
+
+                # PARSE FOR GENERAL FILE FOR PLAYER 2
+                if p2id in secondary_players.keys():
+                    secondary_players[p2id][event] = secondary_players[p2id][event] + 1
+                else:
+                    secondary_players[p2id] = [0] * (13)
+                    secondary_players[p2id][event] = secondary_players[p2id][event] + 1
+
                 # PARSE FOR CONDITIONALS
                 for i in range (len(conditionals)):
                     if (eval(conditionals[i])):
-                        if p1id in conditionalplayers[i].keys():
-                            conditionalplayers[i][p1id][event] = conditionalplayers[i][p1id][event] + 1
+                        if p1id in conditional_primary_players[i].keys():
+                            conditional_primary_players[i][p1id][event] = conditional_primary_players[i][p1id][event] + 1
                         else:
-                            conditionalplayers[i][p1id] = [0] * (6)
-                            conditionalplayers[i][p1id][event] = conditionalplayers[i][p1id][event] + 1
-                
-                
+                            conditional_primary_players[i][p1id] = [0] * (13)
+                            conditional_primary_players[i][p1id][event] = conditional_primary_players[i][p1id][event] + 1
+
+                        if p2id in conditional_secondary_players[i].keys():
+                            conditional_secondary_players[i][p2id][event] = conditional_secondary_players[i][p2id][event] + 1
+                        else:
+                            conditional_secondary_players[i][p2id] = [0] * (13)
+                            conditional_secondary_players[i][p2id][event] = conditional_secondary_players[i][p2id][event] + 1
+
+
 
         # COMPUTE AND WRITE FEATURES
-        y = conditionalplayers
+        z = conditional_primary_players
         with open(sys.argv[4] + data_prefix + data_suffix, 'w') as outfile:
             # ASSUMED FORMAT IS `{pid: [x[0], x[1], x[2], x[3] ...]}`
-            for pid in players.keys():
+            for pid in primary_players.keys():
                 line = str(pid)
                 for feature in features:
-                    x = players[pid]
+                    x = primary_players.get(pid, [])
+                    y = secondary_players.get(pid, [])
                     try:
                         line = line + ', ' + str(eval(feature))
                     except:
